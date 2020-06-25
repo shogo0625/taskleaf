46033323302786345cc59d513fed2b4aabe2508b
shogo0625 <shogo19940625@gmail.com>
2020-06-25 03:43:54 +0000

    ransack検索機能追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,8 @@ class TasksController < ApplicationController
   before_action :set_task, only: %i(show edit update destroy)
 
   def index
-    @tasks = current_user.tasks.recent
+    @q = current_user.tasks.ransack(params[:q])
+    @tasks = @q.result(distinct: true)
   end
 
   def show
@@ -25,7 +26,7 @@ class TasksController < ApplicationController
       render :new
       return
     end
-    
+
     if @task.save
       redirect_to @task, notice: "タスク「#{@task.name}」を登録しました。"
     else
